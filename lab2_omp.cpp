934c7316c0962517337ba7b4c0c4fcd10add8eac
prajapati-sachin <sachinkp1211@gmail.com>
2019-03-17 17:30:35 +0530

    Change dimensions of V and sigma

--- lab2_omp.cpp
+++ lab2_omp.cpp
@@ -31,6 +31,8 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 	svd.shape();
 	pair<matrix, matrix> eigens = svd.eigen();
 	vector<double> eigenvalues = (eigens.first).diagonal();
+	matrix eigenvectors = (eigens.second);
+	eigenvectors.shape();
 	vector<pair<double, int> > eigenv_index;
 	for(int i=0;i<eigenvalues.size();i++){
 		if(abs(eigenvalues[i])>1e-5) eigenv_index.pb(make_pair(eigenvalues[i],i));
@@ -39,12 +41,23 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 	}
 	sort(eigenv_index.begin(), eigenv_index.end());
 	// reverse(eigenv_index.begin(), eigenv_index.end());
-	for(int i=0;i<eigenv_index.size();i++){
-		cout << eigenv_index[i].first << "| ";
-		cout << eigenv_index[i].second << endl;
-	}
+	// for(int i=0;i<eigenv_index.size();i++){
+	// 	cout << eigenv_index[i].first << "| ";
+	// 	cout << eigenv_index[i].second << endl;
+	// }
 	matrix sigma_inv(M, N, 0);
-
+	int e = eigenv_index.size()-1;
+	for(int i=0; i<N;i++){
+		sigma_inv(i,i)= 1/(eigenv_index[e].first);
+		e--;	
+	}
+	sigma_inv.print();
+	matrix v(N, N, 0);
+	e = eigenv_index.size()-1;	
+	// for(int i=0;i<N;i++){
+		// int index = eigenv_index[e].second;
+		// for()
+	// }
 
 }
 
